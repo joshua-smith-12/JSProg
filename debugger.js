e8ad53f42eb77241b9f3b0efc9303d70e60f5268
joshua-smith-12 <55996781+joshua-smith-12@users.noreply.github.com>
2022-12-31 23:21:54 -0600

    adjust debugging output

--- debugger.js
+++ debugger.js
@@ -81,11 +81,10 @@ async function doDebug() {
   }
   
   const chunkData = await fs.readFile(`./chunks/${module}@${version}/chunks.0.wasm`);
-  console.log(chunkData[16]);
-  console.log(chunkData[17]);
-  console.log(chunkData[18]);
   
   const instance = await WebAssembly.instantiate(chunkData, importData);
+  
+  console.log(instance);
 }
 
 doDebug();
