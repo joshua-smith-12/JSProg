f598057477ceebf5521d353d425c06e84c98b3f4
joshua-smith-12 <55996781+joshua-smith-12@users.noreply.github.com>
2023-01-02 11:04:54 -0600

    handle block list terminating before section length

--- jsprog/loader/relocate.js
+++ jsprog/loader/relocate.js
@@ -10,9 +10,12 @@ module.exports = {
         const reloc = sectionTables[relocIndex];
         
         let blockAddr = reloc.dataPointer;
-        while (blockAddr < (reloc.dataPointer + (reloc.addrEnd - reloc.addrStart))) {
+        while (blockAddr < (reloc.dataPointer + (reloc.addrEnd - reloc.addrStart))) { 
             const blockPage = buf.readUInt32LE(blockAddr);
             const blockSize = buf.readUInt32LE(blockAddr + 4);
+            
+            // failsafe
+            if (blockSize === 0) break;
             
             console.log("Processing relocation on page at 0x" + blockPage.toString(16).toUpperCase().padStart(8, '0') + " of block length " + blockSize);
             
