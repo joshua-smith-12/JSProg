1dd99eae97f38e19e6f6b48fec25a5973e25e51b
joshua-smith-12 <55996781+joshua-smith-12@users.noreply.github.com>
2023-01-02 11:32:05 -0600

    apply relocations for 32-bit addresses

--- jsprog/loader/relocate.js
+++ jsprog/loader/relocate.js
@@ -1,3 +1,17 @@
+function relocate(buf, section, address, type, diff) {
+    switch(type) {
+        case 0:
+            console.log("Invalid relocation type 0: used as buffer");
+            break;
+        case 3:
+            // IMAGE_REL_BASED_HIGHLOW
+            const bufferPos = section.dataPointer + (address - section.addrStart);
+            const current = buf.readUInt32LE(bufferPos);
+            buf.writeUInt32LE(current + diff, bufferPos);
+            break;
+    }
+}
+
 module.exports = {
     ApplyRelocations: async function(sectionTables, buf, header, virtualBase = 64 * 1024) {
         console.log("Applying relocations with an image virtual base address of 0x" + virtualBase.toString(16).toUpperCase().padStart(8, '0'));
@@ -22,11 +36,14 @@ module.exports = {
             console.log("Processing relocation on page at 0x" + blockPage.toString(16).toUpperCase().padStart(8, '0') + " of block length " + blockSize);
             
             // read each relocation hint
-            for (let i = 8; i < blockSize; i = i + 2) {
+            for (let i = 8; i < blockSize; i = i + 2) { 
                 const hintPointer = blockAddr + i;
                 const hintValue = buf.readUInt16LE(hintPointer);
+                
                 const relocType = (hintValue & 0xF000) >>> 12;
                 const relocOffset = hintValue & 0x0FFF;
+                
+                if (relocType === 0) continue;
                 
                 const virtualAddress = blockPage + relocOffset + header.optionalHeader.imagePreferredBase;
                 const sectionIndex = sectionTables.findIndex(x => x.addrStart <= virtualAddress && x.addrEnd >= virtualAddress);
@@ -39,7 +56,7 @@ module.exports = {
                 
                 console.log("Applying relocation with type " + relocType + ", virtual address 0x" + virtualAddress.toString(16).toUpperCase().padStart(8, '0'));
             
-                
+                relocate(buf, section, virtualAddress, relocType, relocDiff);
             }
             
             blockAddr = blockAddr + blockSize;
