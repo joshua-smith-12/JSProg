b99b29f7e5e31b8f21e574a7a786cdf2836cc1b9
joshua-smith-12 <55996781+joshua-smith-12@users.noreply.github.com>
2023-01-02 16:46:30 -0600

    fixup some references to esp in output

--- debugger.js
+++ debugger.js
@@ -76,17 +76,17 @@ function inspectMemory(source) {
       break;
   }
   
-  showMemory(inspectBase);
+  showMemory(inspectBase, source);
 }
 
-function showMemory(source) {
+function showMemory(source, input) {
   let rowTop = source;
   for (let i = 0; i < 4; i++) {
     const rowBottom = Math.max(0, rowTop - 16);
     if (rowBottom === rowTop) break;
     
     const stackRow = new Uint32Array(mem.buffer, rowBottom, Math.min(4, (rowTop - rowBottom) / 4));
-    let row = "ESP-0x" + (i * 16).toString(16).toUpperCase().padStart(2, '0') + "  ";
+    let row = input + "-0x" + (i * 16).toString(16).toUpperCase().padStart(2, '0') + "  ";
     for (let j = stackRow.length - 1; j >= 0; j--) {
       const dw = stackRow[j]; 
       row = row + "0x" + dw.toString(16).toUpperCase().padStart(8, '0') + " ";
