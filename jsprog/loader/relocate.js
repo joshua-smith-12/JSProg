ac6d8989eeb5c31a2ee44d292f2b5e2cfb546a0e
joshua-smith-12 <55996781+joshua-smith-12@users.noreply.github.com>
2023-01-02 08:56:04 -0600

    improve debug logging

--- jsprog/loader/relocate.js
+++ jsprog/loader/relocate.js
@@ -1,6 +1,6 @@
 module.exports = {
-    ApplyRelocations: async function(chunks, sectionTables, buf, virtualBase = 16 * 1024 * 1024) {
-        console.log("Applying relocations with an image virtual base address of " + virtualBase.toString(16).toUpperCase().padStart(8, '0'));
+    ApplyRelocations: async function(chunks, sectionTables, buf, virtualBase = 4 * 1024 * 1024) {
+        console.log("Applying relocations with an image virtual base address of 0x" + virtualBase.toString(16).toUpperCase().padStart(8, '0'));
     
         // load the reloc section if it exists
         const relocIndex = sectionTables.findIndex(x => x.name === ".reloc");
