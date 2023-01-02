5f824644d0d8f2624e7f3a8e7ee90a169809f6a4
joshua-smith-12 <55996781+joshua-smith-12@users.noreply.github.com>
2023-01-02 08:41:45 -0600

    improve op debug for imm

--- debugger.js
+++ debugger.js
@@ -117,7 +117,7 @@ function decodeInstruction(instruction) {
       let res = instruction.mnemonic + " "; 
       for(const op of instruction.operandSet) {
         if (op.indirect) res += "[";
-        if (op.type === "imm") res += op.val.toString(16).toUpperCase();
+        if (op.type === "imm") res += "0x" + (op.val | 0).toString(16).toUpperCase();
         else if (op.type === "reg") res += ["eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi"][op.val];
         else console.log(op);
         if (op.indirect) res += "]";
